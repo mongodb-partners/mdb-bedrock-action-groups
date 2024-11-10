cc9fb9367370d51f1fb630b8b437bc994512bb06
Luiz Alves <luiz.alves@mongodb.com>
2024-11-10 17:24:45 +0100

    chore: remove cdk

--- bin/mdb-bedrock-actions.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { MdbBedrockActionsStack } from "../lib/mdb-bedrock-actions-stack";
-
-const app = new cdk.App();
-new MdbBedrockActionsStack(app, "MdbBedrockActionsStack");
